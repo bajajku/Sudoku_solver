faa78f3592b11756302f0c27f21bcd2cd40ad086
bajajku <bajajku@sheridancollege.ca>
2023-09-14 14:24:19 -0400

    created a function to print board on command line.

--- logic.py
+++ logic.py
@@ -7,3 +7,16 @@ board = [[7,8,0,4,0,0,1,2,0],
          [0,7,0,3,0,0,0,1,2],
          [1,2,0,0,0,7,4,0,0],
          [0,4,9,2,0,6,0,0,7]]
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+    for j in range(len(board[0])):
+        if j % 3 == 0 and j != 0 :
+            print("| ", end="")
+
+        if j == 8:
+            print(board[i][j])
+        else:
+            print(str(board[i][j]) + " ", end ="")
