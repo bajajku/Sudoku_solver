5112dd4552be79da2e1642c6be11cc557d77fc0a
bajajku <bajajku@sheridancollege.ca>
2023-09-14 14:28:34 -0400

    created is valid function which will check if the number we are entering is valid or not

--- logic.py
+++ logic.py
@@ -1,4 +1,4 @@
-board = [[7,8,0,4,0,0,1,2,0],
+BOARD = [[7,8,0,4,0,0,1,2,0],
          [6,0,0,0,7,5,0,0,9],
          [0,0,0,6,0,1,0,7,7],
          [0,0,7,0,4,0,2,6,0],
@@ -28,4 +28,28 @@ def find_empty(board):
             if board[i][j] == 0:
                 return (i,j)
     return None
+
+
+def isValid(board, number, pos):
+    
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == number and pos[1] != i:
+            return False
+    
+    # Check columns
+    for i in range(len(board)):
+        if board[i][pos[1]] == number and pos[0] != i:
+            return False
+        
+    # Check 3 x 3 box
+    box_X = pos[1] // 3 # 0
+    box_Y = pos[0] // 3 # 2
+    
+    for i in range(box_Y * 3, box_Y *3 + 3): # range(6,9)
+        for j in range(box_X * 3, box_X * 3 + 3): # range(0,3)
+            if board[i][j] == number and (i,j) != pos:
+                return False
+    
+    return True
 
